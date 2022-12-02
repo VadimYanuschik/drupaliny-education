4153a6b914ba6f679726296a08828b505ea0b3fe
VadimYanuschik <vadim_wower@list.ru>
2022-12-02 17:40:06 +0300

    base class for plugin

---
+++ web/modules/custom/probation/src/Plugin/paragraphs/Behavior/CustomClassesForParagraphs.php
@@ -0,0 +1,11 @@
+<?php
+
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+
+class CustomClassesForParagraphs extends ParagraphsBehaviorBase {
+
+  public function view(array &$build, \Drupal\paragraphs\Entity\Paragraph $paragraph, \Drupal\Core\Entity\Display\EntityViewDisplayInterface $display, $view_mode) {
+    // TODO: Implement view() method.
+  }
+
+}
