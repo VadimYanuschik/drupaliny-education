4700ce6f774b858a1babd1f44d93cf68ad4df8c0
VadimYanuschik <vadim_wower@list.ru>
2022-12-15 14:48:34 +0300

    create queues and jobs

---
+++ web/modules/custom/task_6_cron/src/Plugin/AdvancedQueue/JobType/HabitatImportJob.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\task_6_cron\Plugin\AdvancedQueue\JobType;
+
+use Drupal\advancedqueue\Plugin\AdvancedQueue\JobType\JobTypeBase;
+
+class HabitatImportJob extends JobTypeBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process(\Drupal\advancedqueue\Job $job) {
+    // TODO: Implement process() method.
+  }
+
+}
---
+++ web/modules/custom/task_6_cron/src/Plugin/AdvancedQueue/JobType/TypeImportJob.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\task_6_cron\Plugin\AdvancedQueue\JobType;
+
+use Drupal\advancedqueue\Plugin\AdvancedQueue\JobType\JobTypeBase;
+
+class TypeImportJob extends JobTypeBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process(\Drupal\advancedqueue\Job $job) {
+    // TODO: Implement process() method.
+  }
+
+}
