71ea3f7a7058bb2614a85a2bebb6ce1285c09123
VadimYanuschik <vadim_wower@list.ru>
2022-12-19 18:08:58 +0300

    update pokemon import job

--- web/modules/custom/task_6_cron/src/Plugin/AdvancedQueue/JobType/PokemonImportJob.php
+++ web/modules/custom/task_6_cron/src/Plugin/AdvancedQueue/JobType/PokemonImportJob.php
@@ -3,7 +3,7 @@
 namespace Drupal\task_6_cron\Plugin\AdvancedQueue\JobType;
 
 use Drupal\advancedqueue\Job;
-use Drupal\advancedqueue\Plugin\AdvancedQueue\JobType\JobTypeBase;
+use Drupal\advancedqueue\JobResult;
 
 /**
  * @AdvancedQueueJobType(
@@ -13,12 +13,79 @@ use Drupal\advancedqueue\Plugin\AdvancedQueue\JobType\JobTypeBase;
  *  retry_delay = 10,
  * )
  */
-class PokemonImportJob extends JobTypeBase {
+class PokemonImportJob extends AbstractImportJob {
 
   /**
    * {@inheritdoc}
    */
   public function process(Job $job) {
+    try {
+      $payload = $job->getPayload()[0];
+
+      if (isset($payload)) {
+        $taxonomies = $this->importPokemonTaxonomies($payload);
+
+        $fields = [
+          'type' => 'pokemon',
+          'title' => $payload['name'],
+        ];
+
+        $fields = array_merge($fields, $taxonomies);
+
+        return $this->importEntity(self::STORAGE_NODE, $fields);
+      }
+      return JobResult::failure('no payload');
+    } catch (\Exception $e) {
+      return JobResult::failure($e->getMessage());
+    }
+  }
+
+  /**
+   * Returns list of related taxonomies
+   *
+   * @return string[]
+   */
+  private function getTaxonomiesList(): array {
+    return [
+      'abilities' => 'ability',
+      'colors' => 'color',
+      'egg_groups' => 'egg_group',
+      'forms' => 'form',
+      'genders' => 'gender',
+      'habitats' => 'habitat',
+      'shapes' => 'shape',
+      'species' => 'specie',
+      'types' => 'type',
+    ];
+  }
+
+  /**
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  private function importPokemonTaxonomies(array $payload): array {
+    $taxonomiesList = $this->getTaxonomiesList();
+    $pokemonTaxonomies = [];
+
+    foreach ($taxonomiesList as $taxonomyPlural => $taxonomy) {
+      if (array_key_exists($taxonomyPlural, $payload)) {
+        foreach ($payload[$taxonomyPlural] as $taxonomy_single => $value) {
+          $fields = [
+            'vid' => $taxonomyPlural,
+            'name' => $payload[$taxonomyPlural][$taxonomy_single][$taxonomy]['name'],
+          ];
+
+          $this->importEntity(self::STORAGE_TAXONOMY, $fields);
+          $loaded = $this->loadEntity(self::STORAGE_TAXONOMY, $fields);
+          $taxonomyID = array_pop($loaded)->id();
+
+          $pokemonTaxonomies['field_' . $taxonomy][] = $taxonomyID;
+        }
+      }
+    }
+
+    return $pokemonTaxonomies;
   }
 
 }
