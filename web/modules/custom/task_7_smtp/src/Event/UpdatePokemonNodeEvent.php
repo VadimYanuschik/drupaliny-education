bb485ca7c20accd802ecc1344e08fad74146b7b0
VadimYanuschik <vadim_wower@list.ru>
2022-12-28 17:35:30 +0300

    add getting emails from config form

--- web/modules/custom/task_7_smtp/src/Event/UpdatePokemonNodeEvent.php
+++ web/modules/custom/task_7_smtp/src/Event/UpdatePokemonNodeEvent.php
@@ -8,15 +8,4 @@ class UpdatePokemonNodeEvent extends Event {
 
   const UPDATE_POKEMON = 'task_7_smtp.node_update';
 
-  /**
-   * Mails that will be notified
-   *
-   * @var array
-   */
-  protected array $mails;
-
-  public function __construct() {
-    $this->mails = [];
-  }
-
 }
--- web/modules/custom/task_7_smtp/src/EventSubscriber/UpdatePokemonSubscriber.php
+++ web/modules/custom/task_7_smtp/src/EventSubscriber/UpdatePokemonSubscriber.php
@@ -2,21 +2,56 @@
 
 namespace Drupal\task_7_smtp\EventSubscriber;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\task_7_smtp\Event\UpdatePokemonNodeEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class UpdatePokemonSubscriber implements EventSubscriberInterface {
 
+  /**
+   * @var array
+   */
+  protected array $emails;
+
+  /**
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->emails = $this->getEmails($config_factory);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public static function getSubscribedEvents(): array {
     return [
-      UpdatePokemonNodeEvent::UPDATE_POKEMON => ['sendMailingNotification']
+      UpdatePokemonNodeEvent::UPDATE_POKEMON => ['sendMailingNotification'],
     ];
   }
 
+  /**
+   * Send mailing notification
+   *
+   * @param \Drupal\task_7_smtp\Event\UpdatePokemonNodeEvent $event
+   *
+   * @return void
+   */
   public function sendMailingNotification(UpdatePokemonNodeEvent $event): void {
-    /** @var \Drupal\Core\Messenger\MessengerInterface $messenger */
-    $messenger = \Drupal::service('messenger');
-    $messenger->addMessage('Event for preprocess HTML called');
+
+  }
+
+  /**
+   * Convert email to array
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *
+   * @return array
+   */
+  private function getEmails(ConfigFactoryInterface $config_factory): array {
+    $emails = $config_factory->get('task_7_smtp.mailing_notified_settings')
+      ->get('emails');
+
+    return array_map(fn($email) => trim($email), explode(',', $emails));
   }
 
 }
